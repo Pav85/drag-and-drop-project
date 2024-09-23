fad9b825eb895ffdceae5879959048432c324cf6
Pawel <pawel.werbowy@gmail.com>
2024-09-23 09:25:00 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -14,8 +14,9 @@ class Project {
 }
 
 // Project State Management
+type Listener = (items: Project[]) => void;
 class ProjectState {
-  private listeners: any[] = [];
+  private listeners: Listener[] = [];
   private projects: Project[] = [];
   private static instance: ProjectState;
 
@@ -29,7 +30,7 @@ class ProjectState {
     return ProjectState.instance;
   }
 
-  addListener(listenerFn: Function) {
+  addListener(listenerFn: Listener) {
     this.listeners.push(listenerFn);
   }
 
@@ -131,7 +132,7 @@ class ProjectList {
     this.element = importedNode.firstElementChild as HTMLElement;
     this.element.id = `${this.type}-projects`;
 
-    projectState.addListener((projects: any[]) => {
+    projectState.addListener((projects: Project[]) => {
       this.assignedProjects = projects;
       this.renderProjects();
     });

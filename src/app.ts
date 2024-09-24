bfd90d55d70f4f54077bd407fb6009e0780dcb47
Pawel <pawel.werbowy@gmail.com>
2024-09-24 09:13:47 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -14,13 +14,22 @@ class Project {
 }
 
 // Project State Management
-type Listener = (items: Project[]) => void;
-class ProjectState {
-  private listeners: Listener[] = [];
+type Listener<T> = (items: T[]) => void;
+
+class State<T> {
+  protected listeners: Listener<T>[] = [];
+
+  addListener(listenerFn: Listener<T>) {
+    this.listeners.push(listenerFn);
+  }
+}
+class ProjectState extends State<Project> {
   private projects: Project[] = [];
   private static instance: ProjectState;
 
-  private constructor() {}
+  private constructor() {
+    super();
+  }
 
   static getInstance() {
     if (ProjectState.instance) {
@@ -28,10 +37,6 @@ class ProjectState {
     }
     ProjectState.instance = new ProjectState();
     return ProjectState.instance;
-  }
-
-  addListener(listenerFn: Listener) {
-    this.listeners.push(listenerFn);
   }
 
   addProject(title: string, description: string, numOfPeople: number) {

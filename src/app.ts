4f50494eeb29e74a00288828990a963767f29a14
Pawel <pawel.werbowy@gmail.com>
2024-09-25 10:26:18 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -227,7 +227,10 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
 
   dropHandler(_: DragEvent) {}
 
-  dragLeaveHandler(_: DragEvent) {}
+  dragLeaveHandler(_: DragEvent) {
+    const listEl = this.element.querySelector("ul")!;
+    listEl.classList.remove("droppable");
+  }
 
   configure() {
     this.element.addEventListener("dragover", this.dragOverHandler);

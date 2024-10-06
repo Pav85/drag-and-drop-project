186a87905acc1a1e9085e75938f21f7c5d56fb52
Pawel <pawel.werbowy@gmail.com>
2024-10-06 13:09:10 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,18 @@ module.exports = {
   entry: "./src/app.ts",
   output: {
     filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
-  path: path.resolve(__dirname, "dist"),
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
 };
